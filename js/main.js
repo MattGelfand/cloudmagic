25b3aa3a6ec599150335b98b1e313bc6817a76a0
MattGelfand <matthew.gelfand@gmail.com>
2015-12-03 01:29:42 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// Picture element HTML5 shiv
+
+document.createElement( "picture" );
+
+
+
+
+
+// Smoothscroll CSS-Tricks
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $("html, body").animate({
+          scrollTop: target.offset().top-150
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+
+
+
